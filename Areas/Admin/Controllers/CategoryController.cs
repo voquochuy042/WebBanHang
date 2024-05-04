27168e4251b5ac30797905bc44144329ae71cfc4
VoQuocHuy <voquochuy042@gmail.com>
2024-05-04 12:52:11 +0700

    h

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using WebBanHang.Models;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Admin/Category
+        public ActionResult Index()
+        {
+            var item = db.tb_Category.ToList();
+
+            return View(item);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public ActionResult Add(tb_Category category)
+		{
+            if(ModelState.IsValid)
+            {
+                category.CreateDate = DateTime.Now;
+				category.ModifiedDate = DateTime.Now;
+				db.tb_Category.Add(category);
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(category);
+		}
+        public ActionResult Edit(int id)
+        {
+			var item = db.tb_Category.Find(id);
+			return View(item);
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(tb_Category category)
+		{
+            if (ModelState.IsValid)
+            {
+				
+				db.tb_Category.Attach(category);
+				category.ModifiedDate = DateTime.Now;
+				db.Entry(category).Property(x=>x.Title).IsModified = true;
+				db.Entry(category).Property(x => x.Description).IsModified = true;
+				db.Entry(category).Property(x => x.SeoDescription).IsModified = true;
+				db.Entry(category).Property(x => x.SeoKeyWords).IsModified = true;
+				db.Entry(category).Property(x => x.SeoTitle).IsModified = true;
+				db.Entry(category).Property(x => x.Position).IsModified = true;
+				db.Entry(category).Property(x => x.ModifiedBy).IsModified = true;
+				db.Entry(category).Property(x => x.ModifiedDate).IsModified = true;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(category);
+		}
+		[HttpPost]
+		public ActionResult Delete(int id)
+		{
+			var item = db.tb_Category.Find(id);
+			if(item != null)
+			{
+				db.tb_Category.Remove(item);
+				db.SaveChanges();
+				return Json(new { success = true });
+			}
+			return Json(new { success = false });
+
+		}
+	}
+}
---
+++ Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanHang.Models;
+
+namespace WebBanHang.Areas.Admin.Controllers
+{
+    public class NewsController : Controller
+    {
+        Database1Entities db = new Database1Entities();
+        // GET: Admin/News
+        public ActionResult Index()
+        {
+            var items = db.tb_News.OrderByDescending(x=>x.Id).ToList();
+            return View(items);
+        }
+        public ActionResult Add()
+        {
+			return View();
+		}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public ActionResult Add(tb_News model)
+		{
+            if (ModelState.IsValid)
+            {
+                model.CreateDate = DateTime.Now;
+                model.ModifiedDate = DateTime.Now;
+                db.tb_News.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+			return View(model);
+		}
+        public ActionResult Edit(int id)
+        {
+            var model = db.tb_News.Find(id);
+
+            return View(model);
+        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(tb_News news)
+		{
+			if (ModelState.IsValid)
+			{
+
+				db.tb_News.Attach(news);
+				news.ModifiedDate = DateTime.Now;
+				db.Entry(news).Property(x => x.Title1).IsModified = true;
+				db.Entry(news).Property(x => x.Description).IsModified = true;
+				db.Entry(news).Property(x => x.Image).IsModified = true;
+				db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(news);
+		}
+	}
+}
